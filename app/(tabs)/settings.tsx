69653b7964aa2a9ad088a6c218dab5f0f95bd5d4
mohit-birla-deqode <mbirla@deqode.com>
2024-12-24 18:48:28 +0530

    add new tab

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/ThemedText"
+import { SafeAreaView } from "react-native"
+export default function SettingsTab() {
+
+    return(
+        <SafeAreaView>
+            <ThemedText> Settings tab </ThemedText>
+        </SafeAreaView>
+    )
+}
